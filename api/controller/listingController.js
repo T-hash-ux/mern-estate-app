03af7df3b15ffe1e9788652629f39631bcdadaba
Tyrrance <tyrrancem37@gmail.com>
2024-02-27 14:28:40 -0500

    update header search form functionality

--- api/controller/listingController.js
+++ api/controller/listingController.js
@@ -110,13 +110,14 @@ export const getListings = async (req, res, next) => {
             furnished,
             parking,
             type,
-        }).sort(
-            {[sort]: order}
-        ).limit(limit).skip(startIndex);
+        })
+        .sort({[sort]: order})
+        .limit(limit)
+        .skip(startIndex);
 
         return res.status(200).json(listings);
 
     } catch (error) {
         next(error);
     }
-}
+};
